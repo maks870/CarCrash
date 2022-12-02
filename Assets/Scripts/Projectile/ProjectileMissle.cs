63acbe4e42466fa5e71054b6e4e1adc58abe571a
maks870 <maks7-2014@inbox.ru>
2022-12-02 16:58:53 +0300

    1

--- Assets/Scripts/Projectile/ProjectileMissle.cs
+++ Assets/Scripts/Projectile/ProjectileMissle.cs
@@ -1,57 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ProjectileMissle : Projectile
 {
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float rotateSpeed;
+    [SerializeField] private float maxTimePrediction;
     [SerializeField] private float speed;
-    [SerializeField] private float maxSpeed;
-    [SerializeField] private float angleSpeedChange;
-    [SerializeField] private float stopSpeedMultiplier;
 
-
-    WheelCollider wheel;
-    private Vector3 rotationDir = Vector3.zero;
     protected override void Start()
     {
-        base.Start();
-        wheel = GetComponentInChildren<WheelCollider>();
-        //rb.velocity = (target.transform.position - transform.position).normalized * speed * Time.deltaTime;
-        //startVelocity = rb.velocity.magnitude;
-        Launch();
+        rb = GetComponent<Rigidbody>();
     }
     private void FixedUpdate()
     {
-        Fly();
+        rb.velocity = transform.forward * speed;
+        RotateRocket();
     }
-    private void Launch()
+
+    private void RotateRocket()
     {
-
-    }
-    private void Fly()
-    {
-        //float currentSpeed = speed;
-
-        //Vector3 dir = target.transform.position - transform.position;
-
-        //float rotationAngle = Vector3.Angle(rb.velocity, transform.forward);
-
-        //if (rotationAngle > angleSpeedChange)
-        //    rb.AddForce(-rb.velocity.normalized * currentSpeed * stopSpeedMultiplier * Time.deltaTime, ForceMode.Force);
-
-        //Debug.Log(rotationAngle);
-
-        //if ()
-        //{
-
-        //}
-        //rb.AddForce(transform.forward * currentSpeed * Time.deltaTime, ForceMode.Force);
-        //transform.LookAt(target.transform.position);
-
-        Vector3 dir = target.transform.position - transform.position;
-
-        float rotationAngle = Vector2.Angle(new Vector2(transform.forward.x, transform.forward.z), new Vector2(dir.x, dir.z));
-        wheel.motorTorque = speed;
-        wheel.steerAngle = rotationAngle;
+        var rotation = Quaternion.LookRotation(target.transform.position - transform.position);
+        rb.MoveRotation(Quaternion.RotateTowards(transform.rotation, rotation, rotateSpeed * Time.deltaTime));
     }
 }
