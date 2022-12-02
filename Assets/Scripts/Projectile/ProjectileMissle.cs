7038ad0c6304f026c7e0fb1c36b85f745eb934b5
maks870 <maks7-2014@inbox.ru>
2022-12-02 15:17:12 +0300

    1

--- Assets/Scripts/Projectile/ProjectileMissle.cs
+++ Assets/Scripts/Projectile/ProjectileMissle.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class ProjectileMissle : Projectile
 {
     [SerializeField] private float speed;
+    [SerializeField] private float maxSpeed;
     [SerializeField] private float angleSpeedChange;
     [SerializeField] private float stopSpeedMultiplier;
+
+
+    WheelCollider wheel;
     private Vector3 rotationDir = Vector3.zero;
     protected override void Start()
     {
         base.Start();
+        wheel = GetComponentInChildren<WheelCollider>();
         //rb.velocity = (target.transform.position - transform.position).normalized * speed * Time.deltaTime;
         //startVelocity = rb.velocity.magnitude;
         Launch();
@@ -25,18 +30,28 @@ public class ProjectileMissle : Projectile
     }
     private void Fly()
     {
-        float currentSpeed = speed;
+        //float currentSpeed = speed;
+
+        //Vector3 dir = target.transform.position - transform.position;
+
+        //float rotationAngle = Vector3.Angle(rb.velocity, transform.forward);
+
+        //if (rotationAngle > angleSpeedChange)
+        //    rb.AddForce(-rb.velocity.normalized * currentSpeed * stopSpeedMultiplier * Time.deltaTime, ForceMode.Force);
+
+        //Debug.Log(rotationAngle);
+
+        //if ()
+        //{
+
+        //}
+        //rb.AddForce(transform.forward * currentSpeed * Time.deltaTime, ForceMode.Force);
+        //transform.LookAt(target.transform.position);
 
         Vector3 dir = target.transform.position - transform.position;
 
-        float rotationAngle = Vector3.Angle(transform.forward, dir);
-
-        if (rotationAngle > angleSpeedChange)
-            rb.AddForce(-rb.velocity.normalized * currentSpeed * stopSpeedMultiplier * Time.deltaTime, ForceMode.Force);
-
-        Debug.Log(rotationAngle);
-
-        rb.AddForce(transform.forward * currentSpeed * Time.deltaTime, ForceMode.Force);
-        transform.LookAt(target.transform.position);
+        float rotationAngle = Vector2.Angle(new Vector2(transform.forward.x, transform.forward.z), new Vector2(dir.x, dir.z));
+        wheel.motorTorque = speed;
+        wheel.steerAngle = rotationAngle;
     }
 }
