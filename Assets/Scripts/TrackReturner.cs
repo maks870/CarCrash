e4cf8de8ec17fce9d46ff8929870adfb57d2ae53
maks870 <maks7-2014@inbox.ru>
2022-12-17 12:34:02 +0300

    1

--- Assets/Scripts/TrackReturner.cs
+++ Assets/Scripts/TrackReturner.cs
@@ -2,13 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(ChkTrigger))]
 public class TrackReturner : MonoBehaviour
 {
     [SerializeField] private float timeToReturn = 1;
     [SerializeField] private float returnHeight = 1;
-    [SerializeField] private ChkTrigger chkTrigger;
+    private ChkTrigger chkTrigger;
     private float timeOffTrack = 0;
     private bool onTrack = true;
+    private Rigidbody rbCar;
+
+    private void Start()
+    {
+        chkTrigger = transform.GetComponent<ChkTrigger>();
+        rbCar = transform.parent.GetComponent<Rigidbody>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (rbCar.freezeRotation)
+            rbCar.freezeRotation = false;
+    }
 
     private void OnTriggerStay(Collider other)
     {
@@ -30,7 +44,18 @@ public class TrackReturner : MonoBehaviour
 
     private void ReturnToTrack()
     {
-        transform.parent.transform.position = chkTrigger.lastCheckpoint.position + Vector3.up * returnHeight;
+
+        Quaternion carOffsetAngleY = Quaternion.Euler(0, 90, 0);
+        Quaternion angleReturn = chkTrigger.lastCheckpoint.GetChild(0).rotation * carOffsetAngleY;
+
+        rbCar.velocity = Vector3.zero;
+
+        transform.parent.position = new Vector3(chkTrigger.lastCheckpoint.position.x, 0, chkTrigger.lastCheckpoint.position.z) + Vector3.up * returnHeight;
+        transform.parent.rotation = angleReturn;
+
+        Debug.Log(transform.parent.rotation.eulerAngles);
+        rbCar.freezeRotation = true;
+        transform.parent.GetComponent<Rigidbody>().velocity = Vector3.zero;
         timeOffTrack = 0;
     }
 
