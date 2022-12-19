3d28bdfa8f1370ab5f9985c2708c75ccc2140967
maks870 <maks7-2014@inbox.ru>
2022-12-19 11:36:27 +0300

    1

--- Assets/Scripts/Menu/LootboxMenu/GemPresenterUI.cs
+++ Assets/Scripts/Menu/LootboxMenu/GemPresenterUI.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using YG;
 
 public class GemPresenterUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Text exchangeCoinCost;
+    [SerializeField] private Text exchangeGemReward;
+    [SerializeField] private Text smallGemAd;
+    [SerializeField] private Text mediumGemAd;
+    [SerializeField] private Text mediumAdViewed;
+    [SerializeField] private Text mediumAdViews;
 
-    // Update is called once per frame
-    void Update()
+    public void LoadValuesUI(int exchangeCoinCost, int exchangeGemReward, int smallGemAd, int mediumGemAd, int mediumAdViews)
     {
-        
+        this.exchangeCoinCost.text = exchangeCoinCost.ToString();
+        this.exchangeGemReward.text = exchangeGemReward.ToString();
+        this.smallGemAd.text = smallGemAd.ToString();
+        this.mediumGemAd.text = mediumGemAd.ToString();
+        this.mediumAdViews.text = mediumAdViews.ToString();
+        mediumAdViewed.text = YandexGame.savesData.mediumGemAdViewed.ToString();
+    }
+    public void UpdateDoubleAdUI()
+    {
+        mediumAdViewed.text = YandexGame.savesData.mediumGemAdViewed.ToString();
     }
 }
