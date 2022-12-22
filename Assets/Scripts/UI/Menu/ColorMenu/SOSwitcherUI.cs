f7ad26f224931edf44910057a37fe17575792bd2
maks870 <maks7-2014@inbox.ru>
2022-12-22 16:34:49 +0300

    1

---
+++ Assets/Scripts/UI/Menu/ColorMenu/SOSwitcherUI.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SOSwitcherUI : MonoBehaviour
+{
+    [SerializeField] private GameObject button;
+    [SerializeField] private List<ButtonCollectibleUI> buttons = new List<ButtonCollectibleUI>();
+    [SerializeField] private List<CollectibleSO> carModelsSO = new List<CollectibleSO>();
+}
