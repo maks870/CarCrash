b26f7ba9723bbf52e5933f9922b180ff9eba8558
Daniv112 <2.61.69@mail.ru>
2023-01-07 17:15:53 +0300

    1

--- Assets/Racing Starter Kit/Assets/Scripts/Continue.cs
+++ Assets/Racing Starter Kit/Assets/Scripts/Continue.cs
@@ -16,7 +16,6 @@ public class Continue : MonoBehaviour
         Countdown.SetActive(true);  //countdown UI (3,2,1,go)
         Checkpoints.SetActive(true); //racetrack checkpoints
         LapsSelected.SetActive(true); //turn on the lap requirement race-UI text
-        BotsSelected.SetActive(true); //turns off the bots depending the amount selected by the player
         LapsBotsPanel.SetActive(false); //we turn off the start panel with the laps and bots
         FinishCamera.SetActive(false); //and the camera goes off too, to use the one in the player car
     }
