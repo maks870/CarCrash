0f618bfc6cba7a9d42efb1bac9b7dff64015c365
maks870 <maks7-2014@inbox.ru>
2023-01-27 15:04:20 +0300

    1

--- Assets/Racing Starter Kit/Assets/Scripts/Countdown.cs
+++ Assets/Racing Starter Kit/Assets/Scripts/Countdown.cs
@@ -7,10 +7,13 @@ public class Countdown : MonoBehaviour
     [SerializeField] private GameObject CountDown, LapTimer, CarControls;//race objects that we will use in the coroutine
     [SerializeField] private AudioSource GetReady, GoAudio;//same with audio objects
     [SerializeField] private Sprite[] sprites;
+    [SerializeField] private OverheadLightsSwitcher lightSwitcher;
+
     private Image image;
 
     void Start()
     {
+
         image = CountDown.GetComponent<Image>();
         StartCoroutine(CountStart());//when we hit play in the first menu, this scripts activates and start the countdown coroutine
     }
@@ -18,24 +21,27 @@ public class Countdown : MonoBehaviour
     IEnumerator CountStart()
     {
         //Sets the race time to 0 when the race starts
-        LapTimeManager.MinuteCount = 0;       LapTimeManager.SecondCount = 0;       LapTimeManager.MilliCount = 0;
+        LapTimeManager.MinuteCount = 0; LapTimeManager.SecondCount = 0; LapTimeManager.MilliCount = 0;
         //3, 2, 1 Countdown
         yield return new WaitForSeconds(0.5f);
         image.sprite = sprites[2];//we start with the 3
         GetReady.Play();//play the ready noise (normal pitch bell)
         CountDown.SetActive(true);//and activate it in the game UI
         yield return new WaitForSeconds(1);//after one second
+        lightSwitcher.SwitchLight();
         CountDown.SetActive(false);//turn off the UI
         image.sprite = sprites[1];
         GetReady.Play();//play the ready noise (normal pitch bell)
         CountDown.SetActive(true);//and activate it in the game UI
         yield return new WaitForSeconds(1);//same process for the number 1 after another second
+        lightSwitcher.SwitchLight();
         CountDown.SetActive(false);
         image.sprite = sprites[0];
         GetReady.Play();
         CountDown.SetActive(true);
         //and now that 3 seconds have passed, it's time to start the race
         yield return new WaitForSeconds(1);
+        lightSwitcher.SwitchLight();
         CountDown.SetActive(false);//turn of the countdown UI numbers
         GoAudio.Play();//play the go noise (high pitch bell)
         LapTimer.SetActive(true);//make the race time start running (LapTimeManager.cs script)
---
+++ Assets/Racing Starter Kit/Assets/Scripts/OverheadLightsSwitcher.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverheadLightsSwitcher : MonoBehaviour
+{
+    [SerializeField] private int matIndexInRenderer;
+    [SerializeField] private Material redMat;
+    [SerializeField] private Material yellowMat;
+    [SerializeField] private Material greenMat;
+
+    private int counter = 0;
+    private MeshRenderer meshRenderer;
+
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    public void SwitchLight()
+    {
+        Debug.Log("Switch");
+        Debug.Log(counter);
+        switch (counter)
+        {
+            case 0:
+                meshRenderer.materials[matIndexInRenderer].color = redMat.color;
+                break;
+            case 1:
+                meshRenderer.materials[matIndexInRenderer].color = yellowMat.color;
+                break;
+            case 2:
+                meshRenderer.materials[matIndexInRenderer].color = greenMat.color;
+                break;
+        }
+        counter++;
+    }
+}
