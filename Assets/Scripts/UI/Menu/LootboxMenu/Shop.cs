d023aa7e634abd647551d93fc53f69dca5ae5e68
Daniv112 <2.61.69@mail.ru>
2023-02-01 13:36:26 +0300

    1

--- Assets/Scripts/UI/Menu/LootboxMenu/Shop.cs
+++ Assets/Scripts/UI/Menu/LootboxMenu/Shop.cs
@@ -100,7 +100,6 @@ public class Shop : MonoBehaviour
     {
         if (!EarningManager.SpendGem(lootboxCost))
         {
-            //сообзение о нехватке гемов
             return;
         }
 
