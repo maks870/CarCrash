1cc1a8dbd1fd6ae2a0713eb430caa6ce1cc03823
maks870 <maks7-2014@inbox.ru>
2023-02-01 16:15:05 +0300

    1

--- Assets/Scripts/UI/Menu/StartingTraining.cs
+++ Assets/Scripts/UI/Menu/StartingTraining.cs
@@ -4,77 +4,83 @@ using UnityEngine;
 using UnityEngine.UI;
 
 [Serializable]
-public class Advice
+public class TrainingStage
 {
-    public GameObject adviceObj;
-    public Button button;
+    public RectTransform trainingUI;
+    public RectTransform nonTrainingUI;
+    public Button nextStage;
 
 
     public void Subscribe()
     {
-        button.onClick.AddListener(StartingTraining.NextAdviceAction.Invoke);
+        nextStage.onClick.AddListener(StartingTraining.NextStageAction.Invoke);
     }
 
     public void Unsubscribe()
     {
-        button.onClick.RemoveListener(StartingTraining.NextAdviceAction.Invoke);
+        nextStage.onClick.RemoveListener(StartingTraining.NextStageAction.Invoke);
     }
 }
 
 public class StartingTraining : MonoBehaviour
 {
     [SerializeField] private RectTransform[] permanentsElements;
-    [SerializeField] private List<Advice> advices = new List<Advice>();
+    [SerializeField] private TrainingStage[] trainigStages;
     [SerializeField] private RectTransform trainingTransform;
-
-    // *  //запоминать родителя и элемент для возврата обратно
+    private Transform oldTransfrom;
+    private List<Transform> oldTransforms = new List<Transform>();
 
     private int currentAdvice;
 
-    public static Action NextAdviceAction;
+    public static Action NextStageAction;
 
     public void StartTraining()
     {
-        foreach (RectTransform rectTransform in permanentsElements) 
+        foreach (RectTransform rectTransform in permanentsElements)
         {
-            MoveElement(rectTransform);
+            oldTransforms.Add(rectTransform.parent);
+            rectTransform.parent = trainingTransform;
         }
 
         currentAdvice = 0;
-        NextAdviceAction += OnNextAdvice;
+        NextStageAction += OnNextAdvice;
         OnNextAdvice();
     }
+
     private void OnNextAdvice()
     {
-        if (currentAdvice >= 0 && currentAdvice < advices.Count)
+        if (currentAdvice > 0 && currentAdvice <= trainigStages.Length)
         {
-            advices[currentAdvice].adviceObj.SetActive(true);
-            advices[currentAdvice].Subscribe();
+            if (trainigStages[currentAdvice - 1].nonTrainingUI != null)
+                trainigStages[currentAdvice - 1].nonTrainingUI.parent = oldTransfrom;
+
+            trainigStages[currentAdvice - 1].trainingUI.gameObject.SetActive(false);
+            trainigStages[currentAdvice - 1].Unsubscribe();
         }
 
-        if (currentAdvice > 0 && currentAdvice <= advices.Count)
+        if (currentAdvice >= 0 && currentAdvice < trainigStages.Length)
         {
-            advices[currentAdvice - 1].adviceObj.SetActive(false);
-            advices[currentAdvice - 1].Unsubscribe();
+            if (trainigStages[currentAdvice].nonTrainingUI != null)
+            {
+                oldTransfrom = trainigStages[currentAdvice].nonTrainingUI.parent;
+                trainigStages[currentAdvice].nonTrainingUI.parent = trainingTransform;
+            }
+
+            trainigStages[currentAdvice].trainingUI.gameObject.SetActive(true);
+            trainigStages[currentAdvice].Subscribe();
         }
+
         currentAdvice++;
+
+        if (currentAdvice >= trainigStages.Length)
+            EndTraining();
     }
 
-    private void MoveElement(RectTransform element) 
+    private void EndTraining()
     {
-        element.transform.parent = trainingTransform;
-    }
-
-    private void RevertElement(RectTransform element) 
-    {
-
-    }
-
-    private void EndElement() 
-    {
-        foreach (RectTransform rectTransform in permanentsElements)
+        for (int i = 0; i < permanentsElements.Length; i++)
         {
-            RevertElement(rectTransform);
+            permanentsElements[i].parent = oldTransforms[i]; ;
         }
     }
 }
