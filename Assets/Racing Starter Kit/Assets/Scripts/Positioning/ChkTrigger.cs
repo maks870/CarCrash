b9029ff599dfbc85a669c592df5f7960242bea29
maks870 <maks7-2014@inbox.ru>
2023-02-03 14:15:29 +0300

    1

--- Assets/Racing Starter Kit/Assets/Scripts/Positioning/ChkTrigger.cs
+++ Assets/Racing Starter Kit/Assets/Scripts/Positioning/ChkTrigger.cs
@@ -1,44 +1,16 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ChkTrigger : MonoBehaviour
 {
     //this script takes the count of the checkpoints and laps passed of each player in the race
     //and then calculates it as score with the distance from the last passed checkpoint
     //at the end, the information is processed in ChkManager script (distance, chks and laps passed)
+    private int currentCheckpoint;
+    private int nextChk = 1;
     public static bool startDis;
-    private int currentCheckpoint, nextChk;
     public int CarPosListNumber;
 
     [HideInInspector] public Transform lastCheckpoint;
-
-    private void Start()
-    {
-        if (transform.parent.name.Length == 6)//one-digit number name has 6 characters (for example: AICar4)
-        {
-            CarPosListNumber = int.Parse(transform.parent.name.Substring(transform.parent.name.Length - 1));//get the last character (4)
-        }
-        if (transform.parent.name.Length == 7)//two-digit numbers name has 7 characters (for example: AICar17)
-        {
-            CarPosListNumber = int.Parse(transform.parent.name.Substring(transform.parent.name.Length - 2));//get the last 2 characters (17)
-        }
-        if (CarPosListNumber == 0)
-        {
-            gameObject.name = "CarPos" + CarPosListNumber;
-        }
-        else
-        {
-            if (transform.parent.name.Length == 6)//one-digit number name has 9 characters (for example: CarPosAI4)
-            {
-                gameObject.name = "CarPosAI0" + CarPosListNumber;
-            }
-            if (transform.parent.name.Length == 7)//one-digit number name has 9 characters (for example: CarPosAI4)
-            {
-                gameObject.name = "CarPosAI" + CarPosListNumber;
-            }
-        }
-        nextChk = 1;
-    }
 
     private void OnTriggerEnter(Collider other)
     {
