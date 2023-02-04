875af3f3131de49b50dc250cfc213a2d1951988f
maks870 <maks7-2014@inbox.ru>
2023-02-04 17:11:56 +0300

    1

--- Assets/Scripts/UI/Menu/LootboxMenu/Shop.cs
+++ Assets/Scripts/UI/Menu/LootboxMenu/Shop.cs
@@ -79,7 +79,7 @@ public class Shop : MonoBehaviour
 
     public void InitializeUI()
     {
-        gemPresenterUI.LoadValuesUI(exchangeCoinCost, exchangeGemReward, smallAdReward, mediumAdReward, mediumAdViews);
+        gemPresenterUI.LoadValuesUI(exchangeCoinCost, exchangeGemReward, smallAdReward, mediumAdReward, mediumAdViews, lootboxCost);
     }
 
     public void Exchange()
--- Assets/Scripts/UI/Menu/LootboxMenu/ShopUIController.cs
+++ Assets/Scripts/UI/Menu/LootboxMenu/ShopUIController.cs
@@ -6,15 +6,17 @@ public class ShopUIController : MonoBehaviour
 {
     [SerializeField] private Text exchangeCoinCost;
     [SerializeField] private Text exchangeGemReward;
+    [SerializeField] private Text buyLootboxCost;
     [SerializeField] private Text smallGemAd;
     [SerializeField] private Text mediumGemAd;
     [SerializeField] private Text mediumAdViewed;
     [SerializeField] private Text mediumAdViews;
 
-    public void LoadValuesUI(int exchangeCoinCost, int exchangeGemReward, int smallGemAd, int mediumGemAd, int mediumAdViews)
+    public void LoadValuesUI(int exchangeCoinCost, int exchangeGemReward, int smallGemAd, int mediumGemAd, int mediumAdViews, int buyLootboxCost)
     {
         this.exchangeCoinCost.text = exchangeCoinCost.ToString();
         this.exchangeGemReward.text = exchangeGemReward.ToString();
+        this.buyLootboxCost.text = buyLootboxCost.ToString();
         this.smallGemAd.text = smallGemAd.ToString();
         this.mediumGemAd.text = mediumGemAd.ToString();
         this.mediumAdViews.text = mediumAdViews.ToString();
