a9e4c6d567ba17d49864fe64ec2e363681b9baf4
maks870 <maks7-2014@inbox.ru>
2023-02-05 14:37:44 +0300

    1

---
+++ Assets/Scripts/ActionZone.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityStandardAssets.Vehicles.Car;
+
+public class ActionZone : MonoBehaviour
+{
+    [SerializeField] private UnityEvent StayZoneEvent = new UnityEvent();
+    [SerializeField] private float zoneEventDelay = 1;
+    private Collider userCollider;
+    private float time;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<CarUserControl>() != null)
+        {
+            time = 0;
+            userCollider = other;
+        }
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (userCollider != null && userCollider == other)
+            time += Time.deltaTime;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (userCollider != null && userCollider == other)
+        {
+            time = 0;
+            userCollider = null;
+        }
+    }
+
+    void Update()
+    {
+        if (time >= zoneEventDelay)
+            StayZoneEvent.Invoke();
+    }
+}
