90d2ef46f792be385ab9786474c31564563b61f5
Daniv112 <2.61.69@mail.ru>
2023-02-06 13:17:10 +0300

    1

--- Assets/Scripts/Looker.cs
+++ Assets/Scripts/Looker.cs
@@ -4,17 +4,10 @@ public class Looker : MonoBehaviour
 {
     public Transform target;
 
-    private Vector3 targetVector;
-
     void Update()
     {
-        targetVector = target.position;
 
-        targetVector.x = 0;
-        targetVector.y = 1;
-        targetVector.z = 0;
-       
-        transform.LookAt(targetVector);
+        transform.LookAt(new Vector3(target.position.x, 0, target.position.z));
 
         
 
