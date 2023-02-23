1604e41f7a92d4863fcea21066ba4fd48a743484
Daniv112 <2.61.69@mail.ru>
2023-02-23 17:55:29 +0300

    1

---
+++ Assets/Scripts/Input/MobileButton.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MobileButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+       
+    }
+}
