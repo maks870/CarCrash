5f3b3cc95b3923cbf29c942d2b4fef75aef80b9e
Daniv112 <2.61.69@mail.ru>
2023-03-12 14:16:26 +0300

    1

---
+++ Assets/LanguageTexture.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using YG;
+
+public class LanguageTexture : MonoBehaviour
+{
+    private void Awake()
+    {
+        YandexGame yg = (YandexGame)GameObject.FindObjectOfType<YandexGame>();
+    }
+}
